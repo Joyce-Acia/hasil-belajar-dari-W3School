321f9ea6e234e3d8e35f82aaaf175bdc0b82f68d
Joyce-Acia <aciacia06@gmail.com>
2025-03-30 17:22:59 +0700

    ada jadwal

---
+++ JS/script.js
@@ -0,0 +1,70 @@
+// // <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>My Favorite Things</title>
+//     <!-- CSS  -->
+//     <link rel="stylesheet" href="css\style.css">
+// </head>
+// <header>
+//     <div class="head">
+//         <h1>My Favorite Things(for now)</h1>
+//         <h3><small>Author:Joyce Zaneta</small></h3>
+//         </div>
+// </header>
+// <body class="head">
+//     <form>
+//         <div>
+//             <label for="username">Username:</label><br>
+//         <input type="text" id="username" name="username" required><br>
+
+//         <label for="email">Email:</label><br>
+//         <input type="email" id="email" name="email" required><br>
+
+//         <button type="button" id="submitButton">Submit</button><br>
+
+//             <label for="date">Date of Birth:</label>
+//             <input type="date" id="birth"name="birthday">
+//           </div><br><br>
+
+//         <h5 style="font-size: large;">Was My Web Good?</h5>
+//         <input type="radio" id="html" name="course">
+//         <label style="cursor: pointer;">A. Yes</label>
+
+//         <input type="radio" id="html" name="course">
+//         <label for="html">B. Very</label>
+
+//         <input type="radio" id="html" name="course">
+//         <label for="html">C. Absolutely</label>
+
+
+//         <!-- CHECKBOX -->
+//          <div style="cursor: pointer;">
+//         <h5>What Makes You Think My Web is Good?</h5>
+//         <ul><input type="checkbox" id="html" name="course">
+//         <label for="html">Good Colour Scheme</label></ul>
+//         <ul>
+//         <input type="checkbox" id="html" name="course">
+//         <label for="html">Good Web Design</label>
+//       </ul>
+//       <ul>
+//         <input type="checkbox" id="html" name="course">
+//         <label for="html">Same Favorites</label>
+//       </ul>
+//     </div>
+
+//       <ul>
+//         <input type="checkbox" id="html" name="course">
+//         <label for="html">Pretty Author</label>
+//       </ul>
+    
+//         <textarea rows="2" cols="50" placeholder="for what all these question? kAmU naNyA?">
+//           </textarea>
+
+//         <ul>
+//           <!-- <input type="reset"> -->
+//           <button type="reset" id="submitButton">Reset</button><br>
+//         </ul>
+// </form>
+// {</body>}
